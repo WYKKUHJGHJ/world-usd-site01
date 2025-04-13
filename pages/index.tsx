d2706158e6ed522c7e8e8ce83e4b900422be68d3
ww yyk <98003432+WYKKUHJGHJ@users.noreply.github.com>
2025-04-14 04:41:33 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
-  return <div>首页 Coming Soon...</div>;
-}
 import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>首页</title>
+      </Head>
+      <div>首页 Coming Soon...</div>
+    </>
+  );
+}
 import { GetStaticProps } from "next";
 import { motion } from "framer-motion";
 // 导入多语言内容
