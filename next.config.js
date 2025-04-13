028c022b2044ccd5cdbe4433bdf8b97c3b7eddfc
ww yyk <98003432+WYKKUHJGHJ@users.noreply.github.com>
2025-04-14 04:21:28 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-// next.js config
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["zh", "en"],
+    defaultLocale: "zh"
+  },
+  // 可选：指定自定义构建输出目录或其他 Next 配置
+};
+module.exports = nextConfig;
