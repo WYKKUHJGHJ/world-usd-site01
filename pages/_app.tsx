c30f189e4684f1b7d3f20d728a6e868a7dcf6d92
ww yyk <98003432+WYKKUHJGHJ@users.noreply.github.com>
2025-04-14 05:52:11 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
-      {/* 页面切换动画包装 */}
       <AnimatePresence mode="wait" initial={false}>
-        <Component {...pageProps} key={router.asPath} />
+        <motion.div
+          key={router.asPath}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.4 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
       </AnimatePresence>
     </Layout>
   );
